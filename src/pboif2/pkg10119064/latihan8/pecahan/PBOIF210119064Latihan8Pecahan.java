4963d93fe79d96593e5caf89326835ca3e360e9d
Bagus_24 <Bagus_24@Bagus24>
2020-10-15 19:42:30 +0700

    new commit

---
+++ src/pboif2/pkg10119064/latihan8/pecahan/PBOIF210119064Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini berisi program untuk membandingkan tipe data pecahan
+ */
+public class PBOIF210119064Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
